3a78fc48efad48d82cc73e6d83ff4432fe3bbb66
Ben Ferdinandus <2flyfish@gmail.com>
2020-12-24 12:04:54 +0100

    Changed console engine to sub module

--- ConsoleUI/ConsoleEngine/Models/AnsiAttributes.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ConsoleEngine.Models
-{
-    public class AnsiAttributes
-    {
-        public AnsiColor? FgColor { get; set; }
-        public AnsiColor? BgColor { get; set; }
-    }
-}
--- ConsoleUI/ConsoleEngine/Models/CharacterInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleEngine.Models
-{
-    public class CharacterInfo
-    {
-        public char Glyph { get; set; } = ' ';
-        public AnsiColor? FgColor { get; set; }
-        public AnsiAttributes? Attributes { get; set; }
-    }
-}
